0a5af8761d077a394f3cb40c595ab56ed0ccb8d0
Rp7rf <westhouse51@gmail.com>
2019-10-04 00:39:42 +0900

    add move api

--- lib/dynalist/file_api_client.rb
+++ lib/dynalist/file_api_client.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 require 'json'
-require 'pry'
 
 class FileApiClient < BaseApiClient
-  def get_file_list
+  def get_file
     response = @conn.post "#{@base}file/list", {token: @token}.to_json
     json = JSON.parse(response.body, symbolize_names: true)
     FileTree.instance.root_id = json[:root_file_id]
@@ -14,6 +13,46 @@ class FileApiClient < BaseApiClient
           Document.new(file)
         end
       FileTree.add(instance)
+    end
+  end
+
+  def move_file(queries)
+    changes = queries.map(&:to_query)
+    response = @conn.post "#{@base}file/edit", {token: @token, changes: changes}.to_json
+    JSON.parse(response.body)["results"]
+  end
+
+  class Edit
+    def initialize(file, title)
+      @file = file
+      @title = title
+    end
+
+    def to_query
+      {
+        action: "edit",
+        type: @file.type,
+        file_id: @file.id,
+        title: @title
+      }
+    end
+  end
+
+  class Move
+    def initialize(file, parent_file, index: 0)
+      @file = file
+      @parent_file = parent_file
+      @index = index
+    end
+
+    def to_query
+      {
+        action: "move",
+        type: @file.type,
+        file_id: @file.id,
+        parent_id: @parent_file.id,
+        index: @index
+      }
     end
   end
 end
