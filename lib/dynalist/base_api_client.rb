6c8d3c288fbabc5a02068daa360ed6629459be96
Rp7rf <westhouse51@gmail.com>
2019-10-09 08:15:34 +0900

    change token variable

--- lib/dynalist/base_api_client.rb
+++ lib/dynalist/base_api_client.rb
@@ -5,6 +5,6 @@ class BaseApiClient
   def initialize(token = nil)
     @conn = Faraday.new(:url => 'https://dynalist.io/')
     @base = 'api/v1/'
-    @token = token || ENV['TOKEN']
+    @token = token || ENV['DYNALIST_TOKEN']
   end
 end
